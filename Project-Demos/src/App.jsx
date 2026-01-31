6e15f32e25b3bb1bcf6cef73773a54afda227549
Prabhat Paul <prabhatpaul935@gmail.com>
2026-01-31 12:35:41 +0530

    Accordions & Tesimonials project

--- Project-Demos/src/App.jsx
+++ Project-Demos/src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {accordionData} from "./Component/8.Acordions/Utils/content.js"
+import Accordions from './Component/8.Acordions/Accordions.jsx'
+// import Testamonials from './Component/7.Testimonials/Testamonials'
 // import HiddenSearchBar from './Component/6.HiddenSrchBar/HiddenSearchBar'
 // import ToggleBgColor from './Component/5.ToggleBgColor/ToggleBgColor'
 // import Calculator from './Component/4.Calculator/Calculator'
@@ -15,6 +18,16 @@ const App = () => {
       {/* <Calculator/> */}
       {/* <ToggleBgColor /> */}
       {/* <HiddenSearchBar/> */}
+      {/* <Testamonials/> */}
+      
+      <div className="accordion">
+        {
+          accordionData.map(({title,content,id})=>(
+            <Accordions key={id} title={title} content={content}/>
+          ))
+        }
+      </div>
+      
     </div>
   )
 }
---
+++ Project-Demos/src/Component/8.Acordions/Accordions.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Accordions.css'
+import { useState } from 'react'
+
+const Accordions = ({title,content}) => {
+  const [isActive,setIsActive]=useState(false)
+  return (
+    <section className="accordion-card">
+
+      <div className='header' onClick={()=>setIsActive(!isActive)}>
+        <div>{title}</div>
+        <p className="icon">{isActive?'-':'+'}</p>
+      </div>
+
+      <div className='content'>
+        {isActive && <p className='card-info'>{content}</p>}
+      </div>
+      
+    </section>
+  )
+}
+
+export default Accordions
